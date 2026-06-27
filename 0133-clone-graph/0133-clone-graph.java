3867916efb1c06196722880c3301fc224cacc280
SURBHI PATHAK <surbhi.csit01@gmail.com>
2026-06-28 00:29:28 +0530

    Time: 26 ms (46.91%), Space: 44.2 MB (58.76%) - LeetHub

---
+++ 0133-clone-graph/0133-clone-graph.java
@@ -0,0 +1,29 @@
+class Solution {
+    public Node cloneGraph(Node node) {
+        if (node == null) {
+            return null;
+        }
+
+        Map<Node, Node> map = new HashMap<>();
+        Queue<Node> queue = new LinkedList<>();
+
+        Node clone = new Node(node.val);
+        map.put(node, clone);
+        queue.offer(node);
+
+        while (!queue.isEmpty()) {
+            Node curr = queue.poll();
+
+            for (Node neighbor : curr.neighbors) {
+                if (!map.containsKey(neighbor)) {
+                    map.put(neighbor, new Node(neighbor.val));
+                    queue.offer(neighbor);
+                }
+
+                map.get(curr).neighbors.add(map.get(neighbor));
+            }
+        }
+
+        return clone;
+    }
+}

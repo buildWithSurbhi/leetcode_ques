e47fca2e669c5d95aa26faa7e37b8878d791dd46
SURBHI PATHAK <surbhi.csit01@gmail.com>
2026-08-02 20:56:11 +0530

    Time: 18 ms (79.48%), Space: 55.8 MB (25.23%) - LeetHub

---
+++ 0187-repeated-dna-sequences/0187-repeated-dna-sequences.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public List<String> findRepeatedDnaSequences(String s) {
+        HashSet<String> seen = new HashSet<>();
+        HashSet<String> repeated = new HashSet<>();
+
+        for (int i = 0; i <= s.length() - 10; i++) {
+            String sub = s.substring(i, i + 10);
+
+            if (!seen.add(sub)) {
+                repeated.add(sub);
+            }
+        }
+
+        return new ArrayList<>(repeated);
+    }
+}
